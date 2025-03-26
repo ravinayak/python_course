dd53d383238046060ab0ba1b5c29baaf33f48241
Ravi Kumar Nayak <ravinayak19@gmail.com>
2025-03-26 04:38:08 -0400

    Made milestone project implementation more readable

--- py-code-milestone-1.py
+++ py-code-milestone-1.py
@@ -1,54 +1,62 @@
-menu_str = ''' Please Enter one of the choices (a, b, c): 
+menu_str = '''Please Enter one of the choices (a, b, c): 
 	a. Enter Movie Name to store
 	b. Search for a Movie (Case Insensitive)
 	c. Delete a Movie (Case Insensitive)
-	d. Press q to quit
-'''
+	d. Press q to quit'''
+
 print(menu_str)
+
+user_input = ''
 movies = []
 
-def repeat_input():
+def get_user_input():
   incorrect_input = True
   while incorrect_input:
-    user_input = input('Enter your choice :: ').lower()
+    user_input = input("Enter your choice: ").lower()
     for menu_input in ['a', 'b', 'c', 'q']:
-      if user_input == menu_input:
+      if menu_input == user_input:
         incorrect_input = False
   return user_input
 
-def add_movie(movie_name):
-	movies.append(movie_name)
-	print(f'Movie added')
-	print(f'Current Movies :: {movies}')
- 
-def search_movie(movie_name):
-  print(f'Current Movies :: {movies}')
-  for movie in movies:
-    if movie.lower() == movie_name.lower():
-      return f'Movie exists :: {movie}'
-  return 'Movie does not exist'
+def add_movie(movie):
+	movies.append(movie)
+	print(f'Movie was added, current movies :: {movies}')
+	return movie
 
-def delete_movie(movie_name):
-  for movie in movies:
-    if movie.lower() == movie_name.lower():
-      movies.remove(movie)
-      print(f'Movie deleted :: {movie}')
-      print(f'Current Movies :: {movies}')
+def delete_movie(movie):
+  for movie_in_list in movies:
+    if movie_in_list.lower() == movie.lower():
+      movies.remove(movie_in_list)
+      print(f'Movie removed, current movies :: {movies}')
+      return movie
+  print(f'Movie not removed, current movies :: {movies}')
+  return None
+  
+
+def search_movie(movie):
+  for movie_in_list in movies:
+    if movie_in_list.lower() == movie.lower():
+      print(f'Movie found :: {movie}, current movies :: {movies}')
+      return movie
+  print(f'Movie not found :: {movie}, current movies :: {movies}')
+  return None
 
 def process_user_input(user_input):
   if user_input == 'q':
-    return print('Good Luck')
+    print('Have a good day')
+    return None
+    
+  print(f'Current Movies :: {movies}')
+  movie = input('Enter movie name :: ')
 
-  movie_name = input('Enter Movie Name: ')
   if user_input == 'a':
-    add_movie(movie_name)
+    result_val = add_movie(movie)
   elif user_input == 'b':
-    print(search_movie(movie_name))
+    result_val = search_movie(movie)
   else:
-    delete_movie(movie_name)
-
-user_input = ''
+    result_val = delete_movie(movie)
+    
 while user_input != 'q':
-  user_input = repeat_input()
+  user_input = get_user_input()
   process_user_input(user_input)
   
