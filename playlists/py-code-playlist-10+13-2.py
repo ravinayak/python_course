f8ee9cf8738f6b312855433c31303545d19774a8
Ravi Kumar Nayak <ravinayak19@gmail.com>
2025-07-10 17:48:01 -0400

    unsync, aiohttp installed, generator code as a replacement for thread scheduler

--- playlists/py-code-playlist-10+13-2.py
+++ playlists/py-code-playlist-10+13-2.py
@@ -45,3 +45,56 @@ if __name__ == '__main__':
     greeting = ask_user_queue.get()
     print(f'Greeting :: {greeting}')
     print(f'Time spent in Main Program :: {time.time() - start}')
+    
+# This class is an example of a generator which can be used to run
+# tasks instead of threads
+# Instead of initializing list with an array of elements, we shall
+# initialize list with an array of methods to be executed
+# This list will be looked up by an index, and the method at that index
+# will be returned while the index of the perform_task object will be
+# incremented to return method at next index when next(...) is called on
+# the task_object
+# When we reach the end of the list in task_object, a StopIteration exception
+# will be raised signaling the end of methods in the current perform_task
+# object to be performed
+class PerformTask:
+	def __init__(self, task_num, routines):
+		self.routines = routines
+		self.task_num = task_num
+		self.index = 0
+	
+	def __next__(self):
+		if self.index < len(self.routines):
+			current = self.index
+			self.index += 1
+			return self.routines[current]
+		else:
+			raise StopIteration('No More Elements to return')
+
+task1 = PerformTask(1, list(range(10)))
+task2 = PerformTask(2, list(range(20)))
+task3 = PerformTask(3, list(range(5)))
+
+# All Perform_task objects are included in a master tasks list
+# and are executed one after the other in sequence until each
+# perform_task object is exhausted
+# A perform_task object is removed from the list, executed
+# and then again appended back to the list(if it still contains
+# more tasks to perform)
+# The sequence of execution of task_objects to be executed is:
+#       tasks initial sequence => [1, 3, 2]
+#   [1, 3, 2, 1, 3, 2, 1, 3, 2....]: Same Sequence will repeat
+# until a task is exhausted of all the methods
+
+tasks = [task1, task3, task2]
+
+while tasks:
+	try:
+		task = tasks[0]
+		tasks.remove(task)
+		task_num = task.task_num
+		x = next(task)
+		print(f'Task{task_num} :: {x}')
+		tasks.append(task)
+	except StopIteration as exc:
+		print(f'Task{task_num} finished')
